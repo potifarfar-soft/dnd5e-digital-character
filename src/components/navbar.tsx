6299ea3460f90531230cf5f3f20487f703240f4d
fredrik-hjarner <frederick.hjarner@gmail.com>
2018-07-16 20:17:58 +0200

    .

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,11 +4,9 @@ import { Image, Menu } from "semantic-ui-react";
 
 export const Navbar = () => (
   <Menu fixed="top" inverted>
-    <Menu.Item as="a" header>
-    <Link to="/">
+    <Menu.Item as={Link} to="/" header>
       <Image size="mini" src="/logo.png" style={{ marginRight: "1.5em" }} />
       Potifarfar
-    </Link>
     </Menu.Item>
     <Menu.Item as="a">Link 1</Menu.Item>
     <Menu.Item as="a">Link 2</Menu.Item>
