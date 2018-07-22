d524e9f2216338cbca057e0864696ac243d80ede
fredrik-hjarner <frederick.hjarner@gmail.com>
2018-07-22 23:50:58 +0200

    added 5e-Equipment.ts and a new route to test it.

---
+++ src/routes/eq-test/eq-test.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { Container } from 'semantic-ui-react';
+
+export const EqTest = () => (
+  <Container text>
+    <h1>Equipment test page</h1>
+  </Container>
+);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,2 +1,3 @@
 export { Home } from 'routes/home';
+export { EqTest } from 'routes/eq-test/eq-test';
 export { Landing } from 'routes/landing/landing';
